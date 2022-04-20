d8b98e72877d5b556ec1d4328805a4ee3863f77d
orbservatordev <104023464+orbservatordev@users.noreply.github.com>
2022-04-20 11:49:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("#revo");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 5000,
+  offset: -59,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+const textAnim = TweenMax.fromTo(text, 3, {opacity:1}, {opacity:0});
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 1500,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    .setTween(textAnim)
+    .addTo(controller);
+
+let accelAmount = 0.3;
+let scrollPos =0;
+let delay = 0;
+
+scene.on('update', e=> {
+    scrollPos = e.scrollPos / 1000;
+   
+});
+
+setInterval(() => {
+    delay += (scrollPos - delay) * accelAmount;
+    video.currentTime = delay;
+}, 40); // 33.3 = 1s (en ms) /nb fps ds video (ici 1000/30)
+
